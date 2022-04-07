cbf0e3c570a1d69ec918e0851ffbeb78ca694fed
terence1988 <awstest135@gmail.com>
2022-04-07 14:41:17 +1000

    Cant ignore constant

--- constant/index.ts
+++
@@ -1,4 +0,0 @@
-export const BaseUrl = "https://firestore.googleapis.com/v1";
-export const baseProjectInfo =
-  "/projects/rn-store-app-346200/databases/(default)/documents";
-export const realTimeDB = `https://rn-store-app-346200-default-rtdb.asia-southeast1.firebasedatabase.app`;
